8f56b31da535c6a70def1360e78d3a065176407e
sriram000 <srirammani14@gmail.com>
2021-09-08 05:34:33 +0000

    Dusted.

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,10 @@
 import { React } from 'react';
-import { render, screen } from '@testing-library/react';
-
-// NOTE: automock from jest config doesn't work on apps created with create-react-app.
-jest.mock('./components/simpleButton');
-jest.mock('./core/context', () => ({
-	state: {
-		count: 0,
-		refreshID: 'ABCD',
-	},
-}));
-
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-	render(<App/>);
-	const someText = screen.getByText(/count/i);
+	const component = render(<App/>).getByRole('App');
 
-	expect(someText).toBeInTheDocument();
+	expect(component).toBeInTheDocument();
+	expect(component).toHaveClass('App');
 });
